922bf01d1482f42029095f656b19bdc2fea86ace
walid <walidmh.183@gmail.com>
2023-01-01 00:47:27 +0100

    v1

--- app.js
+++ app.js
@@ -126,6 +126,9 @@ let Temp = {}
 let activeSlideIndex = 0
 slidesArr[activeSlideIndex].querySelector('.slideNameContainer').style.left = 0
 slidesArr[activeSlideIndex].querySelector('.slideShoeImgContainer').style.left = 0
+
+const slidesDescriptions = document.querySelector('.slidesDescriptionsContainer')
+const slideDescription = [...document.querySelectorAll('.slideDescription')]
 function nextSlide() {
    
    activeSlideIndex ++
@@ -137,7 +140,6 @@ function nextSlide() {
    let disActivatedSlideName = activeSlideIndex!==0 ? slidesArr[activeSlideIndex-1].querySelector('.slideNameContainer') : slidesArr[3].querySelector('.slideNameContainer')
    let disActivatedSlideShoe = activeSlideIndex!==0 ?  slidesArr[activeSlideIndex-1].querySelector('.slideShoeImgContainer') : slidesArr[3].querySelector('.slideShoeImgContainer')
 
-   console.log(activeSlideIndex,'/', activeSlideName,'/',activeSlideShoe,'/',disActivatedSlideName,'/',disActivatedSlideShoe);
    activeSlideName.style.left = 0
    activeSlideShoe.style.left = 0
    disActivatedSlideName.style.left = '-100%'
@@ -154,11 +156,8 @@ function nextSlide() {
       }, 200);
 
    }, 1500);
-   function clearInterval1() {
-      clearTimeout(timeout1)
-      
-   }
-   // Temp.fn1 = clearInterval1()
+   changeDescription(activeSlideIndex)
+   
 }
 
 function previousSlide() {
@@ -177,23 +176,12 @@ function previousSlide() {
    let disActivatedSlideName = activeSlideIndex!==3 ? slidesArr[activeSlideIndex+1].querySelector('.slideNameContainer') : slidesArr[0].querySelector('.slideNameContainer')
    let disActivatedSlideShoe = activeSlideIndex!==3 ?  slidesArr[activeSlideIndex+1].querySelector('.slideShoeImgContainer') : slidesArr[0].querySelector('.slideShoeImgContainer')
 
-   console.log(activeSlideIndex,'/', activeSlideName,'/',activeSlideShoe,'/',disActivatedSlideName,'/',disActivatedSlideShoe);
    activeSlideName.style.left = 0
    activeSlideShoe.style.left = 0
    disActivatedSlideName.style.left = '-100%'
    disActivatedSlideShoe.style.left = '100%'
-   // setTimeout(() => {
-   //    disActivatedSlideName.style.transition = 'none'
-   //    disActivatedSlideShoe.style.transition = 'none'
-   //    disActivatedSlideName.style.left = '100%'
-   //    disActivatedSlideShoe.style.left = '-100%'
-   //    setTimeout(() => {
-   //       disActivatedSlideShoe.style.transition = '1.5s cubic-bezier(.66,.25,.23,.99)'
-   //       disActivatedSlideName.style.transition = '1.5s cubic-bezier(.66,.25,.23,.99)'
-         
-   //    }, 1500);
-
-   // }, 2000);
+   changeDescription(activeSlideIndex)
+   
 }
 
 
@@ -224,6 +212,10 @@ function backgroundChange(){
    }
 }
 
+function changeDescription(n) {
+   slidesDescriptions.style.top = (-n*slideDescription[n].offsetHeight ) + 'px'
+}
+
 // shoe animation
 const shoesImages = [...document.querySelectorAll('.slideShoeImg')]
 addEventListener('mousemove',(e) => {
@@ -234,3 +226,88 @@ addEventListener('mousemove',(e) => {
       img.style.transform = `translate(calc(-50% - ${xMoveValue}px), calc(-50% - ${yMoveValue}px))`
    })
 })
+
+const cartComponents = document.querySelector('.cartComponents')
+const totalPriceContainer = document.querySelector('.totalPrice').querySelector('span')
+const taxPriceContainer = document.querySelector('.taxPrice').querySelector('span')
+const totalTaxPriceContainer = document.querySelector('.totalTaxPrice').querySelector('span')
+const cartOpenBtn = document.querySelector('.cartOpenBtn')
+
+let prices = []
+function addToCart(index) {
+   console.log(document.querySelector(`.slide${index+1}Description`).querySelector('.slidePrice').querySelector('span').innerText);
+   let itemPrice = document.querySelector(`.slide${index+1}Description`).querySelector('.slidePrice').querySelector('span').innerText
+   let itemName = [...slidesArr[index].querySelectorAll('.slideName')][0].innerText
+   let newItem = document.createElement('div')
+   newItem.classList.add('item')
+   newItem.innerHTML = `
+   <div class="itemPicture">
+               <img src="images/shoe${index+1}.png" alt="">
+            </div>
+            <div class="itemDescription">
+               <div class="itemName">${itemName}</div>
+               <div class="itemQuantity">Quantity: x<span class="quantity">1</span></div>
+               <div class="itemUnitPrice">price : $<span class="unitPrice">${itemPrice}</span></div>
+                
+            </div>
+   
+   `
+   prices.push(1*(itemPrice))
+   cartComponents.append(newItem)
+   let totalPrice = prices.reduce((a, b) => a + b, 0)
+   let taxPrice = Math.round(totalPrice*0.15*100)/100 
+   let totalTaxPrice = totalPrice + taxPrice
+   totalPriceContainer.innerText = totalPrice
+   taxPriceContainer.innerText = taxPrice
+   totalTaxPriceContainer.innerText = totalTaxPrice
+   let itemsArr = [...document.querySelectorAll('.item')]
+   cartOpenBtn.querySelector('span').innerText = itemsArr.length
+}
+const cart = document.querySelector('.cart')
+function openCart() {
+   cart.classList.add('open')
+}
+function closeCart() {
+   cart.classList.remove('open')
+}
+
+
+addEventListener('click',(e) => {
+   if (!cart.contains(e.target) && !cartOpenBtn.contains(e.target) && cart.classList.contains('open')) {
+      closeCart()
+      console.log('object');
+   }
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// <div class="item">
+//             <div class="itemPicture">
+//                <img src="images/shoe1.png" alt="">
+//             </div>
+//             <div class="itemDescription">
+//                <div class="itemName">Lorem</div>
+//                <div class="itemQuantity">Quantity: x<span class="quantity">2</span></div>
+//                <div class="itemUnitPrice">price : $<span class="unitPrice">749</span></div>
+//                <div class="itemTotalPrice">price : $<span class="totalPrice">1498</span></div>
+//             </div>
+            
+//          </div>
